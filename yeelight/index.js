7204f59677738ac81fa5477813df0023ee955d36
JackZ <emailjiong@126.com>
2017-09-20 22:35:29 +0800

    Add CT Support.

--- yeelight/index.js
+++ yeelight/index.js
@@ -2,225 +2,239 @@ var yeeLight = require('./lib/yee.js');
 var Service, Characteristic, Accessory, UUIDGen;
 
 
-module.exports = function(homebridge) {
-    Accessory = homebridge.platformAccessory;
-    
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
-    
-    homebridge.registerPlatform("homebridge-yeelight", "yeelight", YeePlatform, true);    
+module.exports = function (homebridge) {
+	Accessory = homebridge.platformAccessory;
+
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	UUIDGen = homebridge.hap.uuid;
+
+	homebridge.registerPlatform("homebridge-yeelight", "yeelight", YeePlatform, true);
 }
 
 function YeePlatform(log, config, api) {
-    log("YeePlatform Init");
-    
-    this.log = log;
-    this.config = config;
-    this.yeeAccessories = [];
-    
-    var platform = this;
-    
-    if (api) {
-	this.api = api;
+	log("YeePlatform Init");
 
-	this.api.on('didFinishLaunching', function() {
-	    platform.log("DidFinishLaunching");
-	    
-            platform.yeeAgent = new yeeLight.YeeAgent("0.0.0.0", platform);
-            platform.yeeAgent.startDisc();
-	    
-	}.bind(this));
-    }
+	this.log = log;
+	this.config = config;
+	this.yeeAccessories = [];
+
+	var platform = this;
+
+	if (api) {
+		this.api = api;
+
+		this.api.on('didFinishLaunching', function () {
+			platform.log("DidFinishLaunching");
+
+			platform.yeeAgent = new yeeLight.YeeAgent("0.0.0.0", platform);
+			platform.yeeAgent.startDisc();
+
+		}.bind(this));
+	}
 }
 
 YeePlatform.prototype = {
 
-    onDevFound: function(dev) {
-	var that = this;
-	var uuid;
-	var found = 0;
-	var newAccessory = null;
-	var lightbulbService = null;
-        var name;
-	
-	for (var index in this.yeeAccessories) {
-	    var accessory = this.yeeAccessories[index];
-	    if (accessory.context.did == dev.did) {
-		newAccessory = accessory;
-		found = 1;
-		break;
-	    }
-	}
+	onDevFound: function (dev) {
+		var that = this;
+		var uuid;
+		var found = 0;
+		var newAccessory = null;
+		var lightbulbService = null;
+		var name;
 
-	if (found) {
-	    this.log("cached accessory: " + newAccessory.context.did);
-	    lightbulbService = newAccessory.getService(Service.Lightbulb);
-	} else {
-	    uuid = UUIDGen.generate(dev.did);
-            name = dev.did.name || dev.did.substring(dev.did.length-6);
-            this.log("found dev: " + name); 
-	    newAccessory = new Accessory(name, uuid);
-	    newAccessory.context.did = dev.did;
-	    newAccessory.context.model = dev.model;
-	    lightbulbService = new Service.Lightbulb(name);	    
-	}
+		for (var index in this.yeeAccessories) {
+			var accessory = this.yeeAccessories[index];
+			if (accessory.context.did == dev.did) {
+				newAccessory = accessory;
+				found = 1;
+				break;
+			}
+		}
 
-	
-	dev.ctx = newAccessory;
-	
-	lightbulbService
-	    .getCharacteristic(Characteristic.On)
-	    .on('set', function(value, callback) { that.exeCmd(dev.did, "power", value, callback);})
-	    .value = dev.power;
+		if (found) {
+			this.log("cached accessory: " + newAccessory.context.did);
+			lightbulbService = newAccessory.getService(Service.Lightbulb);
+		} else {
+			uuid = UUIDGen.generate(dev.did);
+			name = dev.did.name || dev.did.substring(dev.did.length - 6);
+			this.log("found dev: " + name);
+			newAccessory = new Accessory(name, uuid);
+			newAccessory.context.did = dev.did;
+			newAccessory.context.model = dev.model;
+			lightbulbService = new Service.Lightbulb(name);
+		}
 
-	if (!found) {
-	    lightbulbService
-		.addCharacteristic(Characteristic.Brightness)
-		.on('set', function(value, callback) { that.exeCmd(dev.did, "brightness", value, callback);})
-		.value = dev.bright;
 
-	    if (dev.model == "color" || dev.model == "stripe" || dev.model == "bedside") {
-		lightbulbService
-		    .addCharacteristic(Characteristic.Hue)
-		    .on('set', function(value, callback) { that.exeCmd(dev.did, "hue", value, callback);})
-	            .value = dev.hue;
+		dev.ctx = newAccessory;
 
 		lightbulbService
-		    .addCharacteristic(Characteristic.Saturation)
-		    .on('set', function(value, callback) { that.exeCmd(dev.did, "saturation", value, callback);})
-	            .value = dev.sat;
-	    }
-	} else {
-	    lightbulbService
-		.getCharacteristic(Characteristic.Brightness)
-		.on('set', function(value, callback) { that.exeCmd(dev.did, "brightness", value, callback);})
-		.value = dev.bright;
+			.getCharacteristic(Characteristic.On)
+			.on('set', function (value, callback) { that.exeCmd(dev.did, "power", value, callback); })
+			.value = dev.power;
 
-	    if (dev.model == "color" || dev.model == "stripe" || dev.model == "bedside") {
-		lightbulbService
-		    .getCharacteristic(Characteristic.Hue)
-		    .on('set', function(value, callback) { that.exeCmd(dev.did, "hue", value, callback);})
-	            .value = dev.hue;
-		
+		if (!found) {
+			lightbulbService
+				.addCharacteristic(Characteristic.Brightness)
+				.on('set', function (value, callback) { that.exeCmd(dev.did, "brightness", value, callback); })
+				.value = dev.bright;
 
-		lightbulbService
-		    .getCharacteristic(Characteristic.Saturation)
-		    .on('set', function(value, callback) { that.exeCmd(dev.did, "saturation", value, callback);})
-	            .value = dev.sat;
-	    }	    
-	}
+			if (dev.model == "color" || dev.model == "stripe" || dev.model == "bedside") {
+				lightbulbService
+					.addCharacteristic(Characteristic.Hue)
+					.on('set', function (value, callback) { that.exeCmd(dev.did, "hue", value, callback); })
+					.value = dev.hue;
 
-	newAccessory.reachable = true;
+				lightbulbService
+					.addCharacteristic(Characteristic.Saturation)
+					.on('set', function (value, callback) { that.exeCmd(dev.did, "saturation", value, callback); })
+					.value = dev.sat;
+			}
 
-	if (!found) {
-	    newAccessory.addService(lightbulbService, name);
-	    this.yeeAccessories.push(newAccessory);
-	    this.api.registerPlatformAccessories("homebridge-yeelight", "yeelight", [newAccessory]);
-	}
-    },
+			if (dev.model == "desklamp") {
+				lightbulbService.addCharacteristic(Characteristic.ColorTemperature).on('set', function (value, callback) {
+					that.exeCmd(dev.did, "temperature", value, callback);
+				}).value = dev.sat;
+			}
+		} else {
+			lightbulbService
+				.getCharacteristic(Characteristic.Brightness)
+				.on('set', function (value, callback) { that.exeCmd(dev.did, "brightness", value, callback); })
+				.value = dev.bright;
 
-    onDevConnected: function(dev) {
-	this.log("accesseory reachable");
+			if (dev.model == "color" || dev.model == "stripe" || dev.model == "bedside") {
+				lightbulbService
+					.getCharacteristic(Characteristic.Hue)
+					.on('set', function (value, callback) { that.exeCmd(dev.did, "hue", value, callback); })
+					.value = dev.hue;
 
-	this.log("dev connected " + dev.did + " " + dev.connected);	
-	var accessory = dev.ctx;
-	accessory.updateReachability(true);	
-    },
 
-    onDevDisconnected: function(dev) {
-	this.log("accesseory unreachable");
+				lightbulbService
+					.getCharacteristic(Characteristic.Saturation)
+					.on('set', function (value, callback) { that.exeCmd(dev.did, "saturation", value, callback); })
+					.value = dev.sat;
+			}
 
-	this.log("dev disconnected " + dev.did + " " + dev.connected);	
-	var accessory = dev.ctx;
+			if (dev.model == "desklamp") {
+				lightbulbService.getCharacteristic(Characteristic.ColorTemperature).on('set', function (value, callback) {
+					that.exeCmd(dev.did, "temperature", value, callback);
+				}).value = dev.sat;
+			}
+		}
 
-	// updateReachability seems have bug, but remove the accessory will cause
-	// the name of the light gone, leave the user to decide...
-	if (1) {
-	    accessory.updateReachability(false);	    
-	} else {
-	    this.api.unregisterPlatformAccessories("homebridge-yeelight", "yeelight", [accessory]);
+		newAccessory.reachable = true;
 
-	    var idx = this.yeeAccessories.indexOf(accessory);
-	    if (idx > -1) {
-		this.yeeAccessories.splice(idx, 1);
-	    }
+		if (!found) {
+			newAccessory.addService(lightbulbService, name);
+			this.yeeAccessories.push(newAccessory);
+			this.api.registerPlatformAccessories("homebridge-yeelight", "yeelight", [newAccessory]);
+		}
+	},
 
-	    this.yeeAgent.delDevice(dev.did);
-	}
-    },
+	onDevConnected: function (dev) {
+		this.log("accesseory reachable");
 
-    onDevPropChange: function(dev, prop, val) {
-        var accessory = dev.ctx;
-        var character;
-        var lightbulbService = accessory.getService(Service.Lightbulb);
+		this.log("dev connected " + dev.did + " " + dev.connected);
+		var accessory = dev.ctx;
+		accessory.updateReachability(true);
+	},
 
-        this.log("update accessory prop: " + prop + "value: " + val);
+	onDevDisconnected: function (dev) {
+		this.log("accesseory unreachable");
 
-        if (prop == "power") {
-            character = lightbulbService.getCharacteristic(Characteristic.On)
-        } else if (prop == "bright") {
-            character = lightbulbService.getCharacteristic(Characteristic.Brightness)
-        } else if (prop == "sat") {
-            character = lightbulbService.getCharacteristic(Characteristic.Saturation)
-        } else if (prop == "hue") {
-            character = lightbulbService.getCharacteristic(Characteristic.Hue)
-        } else {
-            return;
-        }
-        character.updateValue(val);
-    },
+		this.log("dev disconnected " + dev.did + " " + dev.connected);
+		var accessory = dev.ctx;
 
-    configureAccessory: function(accessory) {
-	var platform = this;
+		// updateReachability seems have bug, but remove the accessory will cause
+		// the name of the light gone, leave the user to decide...
+		if (1) {
+			accessory.updateReachability(false);
+		} else {
+			this.api.unregisterPlatformAccessories("homebridge-yeelight", "yeelight", [accessory]);
 
-	//accessory.updateReachability(false);
-	accessory.reachable = true;
+			var idx = this.yeeAccessories.indexOf(accessory);
+			if (idx > -1) {
+				this.yeeAccessories.splice(idx, 1);
+			}
 
-	accessory.on('identify', function(paired, callback) {
-            platform.log("identify ....");
-	});
+			this.yeeAgent.delDevice(dev.did);
+		}
+	},
 
-	this.yeeAccessories.push(accessory);
-	
-	return;
-    },
+	onDevPropChange: function (dev, prop, val) {
+		var accessory = dev.ctx;
+		var character;
+		var lightbulbService = accessory.getService(Service.Lightbulb);
 
-    exeCmd: function(did, characteristic, value, callback) {
+		this.log("update accessory prop: " + prop + "value: " + val);
 
-        dev = this.yeeAgent.getDevice(did);
+		if (prop == "power") {
+			character = lightbulbService.getCharacteristic(Characteristic.On)
+		} else if (prop == "bright") {
+			character = lightbulbService.getCharacteristic(Characteristic.Brightness)
+		} else if (prop == "sat") {
+			character = lightbulbService.getCharacteristic(Characteristic.Saturation)
+		} else if (prop == "hue") {
+			character = lightbulbService.getCharacteristic(Characteristic.Hue)
+		} else {
+			return;
+		}
+		character.updateValue(val);
+	},
 
-        if (dev == null) {
-            this.log("no device found for did: " + did);
-            return;
-        }
+	configureAccessory: function (accessory) {
+		var platform = this;
 
-	switch(characteristic.toLowerCase()) {
-	    
-	case 'identify':
-            this.log("identfy....");
-	    dev.setBlink();
-	    break;
-	case 'power':
-	    dev.setPower(value);
-	    break;
-	case 'hue':
-	    dev.setColor(value, dev.sat);
-	    break;
-	case 'brightness':
-	    dev.setBright(value);
-	    break;
-	case 'saturation':
-	    dev.setColor(dev.hue, value);
-	    break;
-	default:
-	    break;
-	}
+		//accessory.updateReachability(false);
+		accessory.reachable = true;
 
-	if (callback)
-	    callback();
-    },
-    
+		accessory.on('identify', function (paired, callback) {
+			platform.log("identify ....");
+		});
+
+		this.yeeAccessories.push(accessory);
+
+		return;
+	},
+
+	exeCmd: function (did, characteristic, value, callback) {
+
+		dev = this.yeeAgent.getDevice(did);
+
+		if (dev == null) {
+			this.log("no device found for did: " + did);
+			return;
+		}
+
+		switch (characteristic.toLowerCase()) {
+
+			case 'identify':
+				this.log("identfy....");
+				dev.setBlink();
+				break;
+			case 'power':
+				dev.setPower(value);
+				break;
+			case 'hue':
+				dev.setColor(value, dev.sat);
+				break;
+			case 'brightness':
+				dev.setBright(value);
+				break;
+			case 'saturation':
+				dev.setColor(dev.hue, value);
+				break;
+			case 'temperature':
+				dev.setTemp(value);
+			default:
+				break;
+		}
+
+		if (callback)
+			callback();
+	},
+
     /*
     configurationRequestHandler : function(context, request, callback) {
 	this.log("Context: ", JSON.stringify(context));
